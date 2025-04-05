fd92315db4c2c94bcbab26593e88c7c04de34791
HelloDoom113 <hellodoom92@gmail.com>
2025-04-05 15:27:32 +0300

    Update animekai.js

--- src/animekai.js
+++ src/animekai.js
@@ -33,13 +33,22 @@ class AnimeKaiScraper {
     this.searchUrl = SRC_SEARCH_URL;
     
     // Configure axios defaults
-    this.client = axios.create({
-      headers: {
-        "User-Agent": USER_AGENT,
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br"
-      }
-    });
+   this.client = axios.create({
+  headers: {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "Cache-Control": "max-age=0"
+  },
+  timeout: 10000
+});
   }
 
   /**
